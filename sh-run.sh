9ecac436b83946c54b77ae80b50f9542b25c57e5
Gregory Petri <gcpetri@outlook.com>
2022-02-23 18:42:14 -0600

    updates

--- sh-run.sh
+++ sh-run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+docker-compose down
 docker-compose up -d
 
-export FLASK_APP=app
+export FLASK_APP=run.py
 export FLASK_ENV=development
 
-flask db init
-flask db migrate
-flask db upgrade
+#flask db init
+#flask db migrate
+#flask db upgrade
 
 flask run
