bf493640b8b17a18934757d906cf89b9155a46e5
Gregory Petri <gcpetri@outlook.com>
2022-03-03 15:27:21 -0600

    seed data still failing

--- app/db/seed/data.py
+++ app/db/seed/data.py
@@ -12,9 +12,9 @@ from app.db.models.Song import Song
 def seed():
     s = db.session()
 
-    if Member.query.filter(Member.email=='gcpetri@tamu.edu').first() != None: 
-        # seed data is already present
+    if len(Member.query.filter(Member.email=='gcpetri@tamu.edu').all()) == 1: # seed data is already present
         return
+    print('inserting seed data')
 
     member1 = Member('gcpetri@tamu.edu', 'Greg', 'Petri')
     member2 = Member('vitruong00@tamu.edu', 'Vi', 'Truong')
