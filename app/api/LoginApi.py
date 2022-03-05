eab5d1eadbb5bae84f744a827d448b9225ddd09c
Gregory Petri <gcpetri@outlook.com>
2022-03-05 14:37:42 -0600

    login updates

--- app/api/LoginApi.py
+++ app/api/LoginApi.py
@@ -1,9 +1,6 @@
 from flask import jsonify, make_response, request, session
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from app.services.MemberService import deleteMember, addMember, getByEmail, getById
-
-# Define parser and request args
-parser = reqparse.RequestParser()
 
 class LoginApi(Resource):
 
@@ -13,13 +10,13 @@ class LoginApi(Resource):
         try:
             memberId = request.headers['MEMBERID']
             print('memberId', memberId)
-            return jsonify({})
+            return jsonify(getById(memberId))
         except:
             print('logging in')
             member = getByEmail(data['email'])
             if member == None:
                 member = addMember(memberId, data['email'], data['firstname'], data['lastname'])
             print('memberId', member.memberId)
-            resp = make_response(jsonify({}), 200)
+            resp = make_response(jsonify(member), 200)
             resp.set_cookie('memberId', value=str(member.memberId))
             return resp
