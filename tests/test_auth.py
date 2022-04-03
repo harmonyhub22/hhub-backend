80809b172c1f3e987f17d9198c9ea7c73d2b372d
Dean Orenstein <dto3576@gmail.com>
2022-04-03 13:04:15 -0500

    more auth test, install pytest-order to establish ordering of tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,21 +3,36 @@ import pytest
 '''
 Route: api/signup
 REST operation: POST
-Service method tested: addMember()
+Service methods tested: addMember(), getByMemberId(), addOrUpdateAuth(), generateToken()
 Cases to test:
+1. if user is not a member, create them and authenticate them. check queryset and hhub-token
+2. user is a member and tries signing up again, so they must log in (catch BadRequestException)
 '''
+@pytest.mark.order(1)
 def testSignup(client):
+    assert client.get()
     pass
 
 '''
 Route: api/login
-REST operation: POST
-Service method tested: addMember()
+REST operation: PUT
+Service methods tested: getByMemberId(), generateToken()
 Cases to test:
+1. email is not provided 
+2. 
 '''
+@pytest.mark.order(2)
 def testLogin(client):
     pass
 
+'''
+Route: api/logout
+REST operation: POST
+Service methods tested: none (just deletes cookie)
+Cases to test:
+1. normal logout, assert 200 response
+'''
+@pytest.mark.order(3)
 def testLogout(client, auth):
     pass
     '''
