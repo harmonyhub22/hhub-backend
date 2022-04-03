2d7027d9fcbcf57b4f0aa035c10d0abc2db617cb
Dean Orenstein <dto3576@gmail.com>
2022-04-03 15:46:19 -0500

    no turn count on session

--- app/db/models/Session.py
+++ app/db/models/Session.py
@@ -16,7 +16,6 @@ class Session(db.Model):
     __table_args__ = {'schema':os.getenv('SCHEMA', 'public')}
 
     sessionId: uuid
-    turnCount: int
     startTime: DateTime
     endTime: DateTime
     member1: Member
@@ -27,7 +26,6 @@ class Session(db.Model):
     sessionId = db.Column('session_id', UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     member1Id = db.Column('member1_id', UUID(as_uuid=True), db.ForeignKey(Member.memberId), nullable=False)
     member2Id = db.Column('member2_id', UUID(as_uuid=True), db.ForeignKey(Member.memberId), nullable=False)
-    turnCount = db.Column('turn_count', db.Integer, nullable=False, default=0)
     startTime = db.Column('start_time', db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     endTime = db.Column('end_time', db.DateTime)
     member1 = db.relationship('Member', uselist=False, foreign_keys=[member1Id])
