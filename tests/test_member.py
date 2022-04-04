4c4676e68d531adb53d2e950eebb3c451195ec68
Olumayowa Olowomeye <52811635+Mav-ert@users.noreply.github.com>
2022-04-04 09:16:11 -0500

    get member test

--- tests/test_member.py
+++ tests/test_member.py
@@ -1,7 +1,7 @@
 import pytest
 from app.db.db import db
 from app.db.models.Member import Member
-
+import uuid
 '''
 Route: api/members/<member ID>
 REST operation: GET
@@ -10,5 +10,17 @@ Cases to test:
 1. trying to get a member with an invalid member ID. check that queryset is empty
 2. normal case: valid member ID 
 '''
-def testGetMemberById(client, app):
+def testGetMemberById(client, app,auth):
+    auth.login()
+    #case 1
+    nullid = uuid.UUID('28cf2179-74ed-0000-0000-3c09bd904365')
+    with app.app_context():
+        query = Member.query.get(nullid)
+        assert not query
+    #case 2
+    deanId = uuid.UUID('28cf2179-74ed-4fab-a14c-3c09bd904365')
+    with app.app_context():
+        query = Member.query.get(deanId)
+        assert len(query) == 1
+        assert query[0].member_id == 0
     pass
